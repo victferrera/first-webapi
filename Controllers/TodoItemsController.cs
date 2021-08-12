f73d58cc541b58ea78f5481adc2c84f659cde48a
= <victor-bash2@hotmail.com>
2021-08-11 22:21:56 -0300

    Added Summary on methods

--- Controllers/TodoItemsController.cs
+++ Controllers/TodoItemsController.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using FirstWebApi.Context;
 using FirstWebApi.Models;
-using Newtonsoft;
 
 namespace FirstWebApi.Controllers
 {
